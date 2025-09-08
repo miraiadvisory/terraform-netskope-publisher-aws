3139c9fdd67f5082ed9ea5cb57611de403ee688f
javier arranz <javier.arranz@mirairisktech.com>
2025-09-08 12:11:26 +0200

    outputs needed

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 //Publisher Details
-# output "publisher_name" {
-#   description = "Name of the Publisher"
-#   value       = netskope_npa_publisher.Publisher.common_name
-# }
+output "publisher_name" {
+  description = "Name of the Publisher"
+  value       = netskope_npa_publisher.Publisher.common_name
+}
 
-# output "publisher_id" {
-#   description = "ID of the Publisher"
-#   value       = netskope_npa_publisher.Publisher.publisher_id
-# }
+output "publisher_id" {
+  description = "ID of the Publisher"
+  value       = netskope_npa_publisher.Publisher.publisher_id
+}
 
 # output "publisher_public_ip" {
 #   description = "Public IP of the Publisher"
